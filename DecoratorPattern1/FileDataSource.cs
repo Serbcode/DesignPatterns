3acda86272c61373743d7f3a8ce9258afa5db4bb
Serge Bazunoff <sergey.bazunoff@gmail.com>
2021-03-12 23:19:23 +0300

    Decorator pattern example 1

---
+++ DecoratorPattern1/FileDataSource.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace DecoratorPattern1
+{
+    // Один из конкретных компонентов реализует базовую
+    // функциональность. Мы не можем, например, ее менять (чужой код) но нужно...
+
+    public class FileDataSource : DataSource
+    {
+        private readonly string filename;
+
+        public FileDataSource(string filename)
+        {
+            this.filename = filename ?? throw new ArgumentNullException(nameof(filename));
+        }
+
+        public override string ReadData()
+        {
+            return File.ReadAllText(filename);
+        }
+
+        public override void WriteData(string data)
+        {
+            File.WriteAllText(filename, data);
+        }
+    }
+}
---
+++ DecoratorPattern1/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+
+namespace DecoratorPattern1
+{
+    // Создаем декоратор.
+    // это родитель всех декораторов. он наследует и аггрегирует общий интерфейс!
+    
+    // Родитель всех декораторов содержит код обёртывания.
+    public class DataSourceDecorator : DataSource
+    {
+        private readonly DataSource source;
+
+        public DataSourceDecorator(DataSource source)
+        {
+            this.source = source ?? throw new ArgumentNullException(nameof(source));
+        }
+
+        public override string ReadData()
+        {
+            return source.ReadData();
+        }
+
+        public override void WriteData(string data)
+        {
+            source.WriteData(data);
+        }
+    }
+
+    public class EncryptionDecorator : DataSourceDecorator
+    {
+        public EncryptionDecorator(DataSource dataSource) : base(dataSource)
+        {
+
+        }
+
+        public override string ReadData()
+        {
+            string encrypted = base.ReadData();
+            string decrypted = encrypted.Substring(1, encrypted.Length - 2);
+            return decrypted;
+        }
+
+        public override void WriteData(string data)
+        {
+            data = "#" + data + "#";
+            base.WriteData(data);
+        }
+    }
+
+    public class CompressionDecorator : DataSourceDecorator
+    {
+        public CompressionDecorator(DataSource dataSource) : base(dataSource)
+        {
+        }
+
+        public override void WriteData(string data)
+        {
+            data = ">" + data + "<";
+            base.WriteData(data);
+        }
+
+        public override string ReadData()
+        {
+            string zipped = base.ReadData();
+            string unzipped = zipped.Substring(1, zipped.Length - 2);
+            return unzipped;
+        }
+    }
+
+    public class Client
+    {
+        // Клиентский код работает со всеми объектами, используя интерфейс
+        // Компонента. Таким образом, он остаётся независимым от конкретных
+        // классов компонентов, с которыми работает.
+        public void ClientCode(DataSource component)
+        {
+            component.WriteData("here is a text data");
+            Console.WriteLine(component.ReadData());            
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            var fs = new FileDataSource("Test.txt");
+            client.ClientCode(fs);
+
+            // а теперь декораторы.
+            // декораторы могут обертывать декораторы ))
+            EncryptionDecorator ed = new EncryptionDecorator(fs);
+            CompressionDecorator cd = new CompressionDecorator(ed);
+            // клиентский код работает также с ними.
+            client.ClientCode(cd);
+        }
+    }
+}
