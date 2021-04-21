3ec58348cf34af29c68840b478f1414107f8cfac
Serge Bazunoff <sergey.bazunoff@gmail.com>
2021-04-21 18:00:06 +0300

    Action delegate exam

---
+++ DelegatesAction/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DelegatesAction
+{
+    class Program
+    {
+        /*
+        Action is a delegate type defined in the System namespace. 
+        An Action type delegate is the same as Func delegate except that the Action delegate doesn't return a value. 
+        In other words, an Action delegate can be used with a method that has a void return type.
+        
+        public delegate void Action<in T>(T obj);
+
+         */
+
+        static void Main(string[] args)
+        {
+            Action<int> printActionDelegate = ConsolePrint;
+            printActionDelegate(10);
+        }
+
+        static void ConsolePrint(int i)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
